aff4eba266b097c17ffe5fb6949ec858e1f7630e
Monika1012 <58320467+Monika1012@users.noreply.github.com>
2020-12-13 23:15:52 +1300

    Basic seller profile scenarios added. steps added. skeleton ready

---
+++ MarsQA-1/Feature/SellerProfile.feature
@@ -0,0 +1,64 @@
+﻿Feature: Seller Profile Feature Operations
+	As a Seller
+	I want the feature to add, edit my Profile Details
+	So that
+	The people seeking for some skills can look into my details.
+
+
+
+@Regression
+Scenario: Add a new seller profile successfully
+	Given I login to the website with <usename> and <password>
+	And Ensure the profile page loads
+	When I Click on New Profile button
+	Then New Profile form opens up
+	When I enter following details in the form and click save button
+	| username | password | title | job | profileName |
+	| wow      | wow1     | QA    | FT  | hello1      |
+	Then Details Saved message appears
+	And The new created profiles shows up in the grid with profile name <profileName>
+
+@Regression
+Scenario: Add multiple(2) seller seller profiles successfully
+	Given I login to the website with <usename> and <password>
+	And Ensure the profile page loads
+	When I enter following details in the form and click save button
+	| username | password | title | job | profileName |
+	| wow      | wow1     | QA    | FT  | hello1      |
+	Then Details Saved message appears
+	And The new created profiles shows up in the grid with profile name <profileName1>
+	When I Click on New Profile button
+	Then New Profile form opens up
+	When I enter following details in the form and click save button
+	| username | password | title | job | profileName2 |
+	| wow      | wow12    | QA    | FT  | hello2     |
+	Then Details Saved message appears
+	Then The new created profiles shows up in the grid with profile name <profileName2>
+	
+@Regression
+Scenario: Edit a seller profile successfully
+	Given I login to the website with <usename> and <password>
+	When I Select the profile <profileName>
+	And I edit following details in the form and click save button
+	| username | password | title | job | profileName |
+	| wow      | wow1     | QA    | FT  | hello1      |
+	Then Details Saved message appears
+	Then The new created profiles shows up in the grid with profile name <profileName>
+
+@Regression
+Scenario: Delete a seller profile successfully
+	Given I login to the website with <usename> and <password>
+	When I Select the profile <profileName>
+	And Click Delete button
+	Then Profile deleted message appears
+	Then The <profileName> profile does not appear on the grid.
+
+@Regression
+Scenario: Search and view an existing seller profile successfully
+	Given I login to the website with <usename> and <password>
+	When I Select the profile <profileName>
+	And Click view button
+	Then Selected profile opens up on the page
+	Then I am able to validate all critical profile details successfully
+	| title | job |
+	| QA    | FT  |
---
+++ MarsQA-1/Steps/AddProfileDetailSteps.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Steps {
+    [Binding]
+    public sealed class AddProfileDetailSteps {
+        [Given(@"I login to the website with (.*) and (.*)")]
+        public void GivenILoginToTheWebsiteWithAnd(string username, string password) {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"Ensure the profile page loads")]
+        public void GivenEnsureTheProfilePageLoads() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter following details in the form and click save button")]
+        public void WhenIEnterFollowingDetailsInTheFormAndClickSaveButton(Table table) {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Details Saved message appears")]
+        public void ThenDetailsSavedMessageAppears() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"The new created profiles shows up in the grid with profile name (.*)")]
+        public void ThenTheNewCreatedProfilesShowsUpInTheGridWithProfileName(string p0) {
+            ScenarioContext.Current.Pending();
+        }
+        //New Stuff
+        [When(@"I Click on New Profile button")]
+        public void WhenIClickOnNewProfileButton() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"New Profile form opens up")]
+        public void ThenNewProfileFormOpensUp() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I Select the profile (.*)")]
+        public void WhenISelectTheProfile(string p0) {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I edit following details in the form and click save button")]
+        public void WhenIEditFollowingDetailsInTheFormAndClickSaveButton(Table table) {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"Click Delete button")]
+        public void WhenClickDeleteButton() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Profile deleted message appears")]
+        public void ThenProfileDeletedMessageAppears() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"The (.*) profile does not appear on the grid\.")]
+        public void ThenTheProfileDoesNotAppearOnTheGrid_(string p0) {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"Click view button")]
+        public void WhenClickViewButton() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Selected profile opens up on the page")]
+        public void ThenSelectedProfileOpensUpOnThePage() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I am able to validate all critical profile details successfully")]
+        public void ThenIAmAbleToValidateAllCriticalProfileDetailsSuccessfully(Table table) {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
